6252ebb69061eba33f49db90bf08c2b33a6c1bf5
ahora111 <nimbuz72@gmail.com>
2025-04-14 00:13:14 +0330

    Update main.py

--- main.py
+++ main.py
@@ -203,7 +203,7 @@ def prepare_final_message(category_name, category_lines, update_date):
 
     # ادغام هدر، خطوط قالب‌بندی‌شده و فوتر
     footer = "\n\n☎️ شماره های تماس:\n📞 09371111558\n📞 02833991417"
-    final_message = f"{header}{'\n\n'.join(formatted_lines)}{footer}"
+    final_message = f"{header}" + "\n\n".join(formatted_lines) + f"{footer}"
 
     return final_message
 # این تابع کمکی برای گرفتن اسم دسته‌بندی‌ها
