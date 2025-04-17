4d17fd79758bff2bfb373d4254032f21fc20c423
ahora111 <nimbuz72@gmail.com>
2025-04-17 18:22:32 +0330

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from persiantools.jdatetime import JalaliDate
+from oauth2client.service_account import ServiceAccountCredentials
 
 
 # --- تنظیمات Google Sheets ---
