eeb1a6f8e6654bbb1ed979fe2d3282ef781bdc21
ahora111 <nimbuz72@gmail.com>
2025-04-21 01:31:26 +0330

    Update main.py

--- main.py
+++ main.py
@@ -28,20 +28,20 @@ CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
-
 # منطقه زمانی ایران
 iran_tz = pytz.timezone('Asia/Tehran')
 now = datetime.now(iran_tz)
 current_time = now.time()
-weekday = now.weekday()  # 0=دوشنبه، ..., 4=پنج‌شنبه، 5=جمعه, 6=شنبه
 
-# بازه مجاز در روزهای عادی
+# بازه مجاز اجرا
 start_time = dt_time(9, 30)
 end_time = dt_time(23, 30)
 
-    if not (start_time <= current_time <= end_time):
-        print("🕒 خارج از بازه مجاز اجرا (۹:۳۰ تا ۲۳:۳۰). اسکریپت متوقف شد.")
-        sys.exit()
+# بررسی
+if not (start_time <= current_time <= end_time):
+    print("🕒 خارج از بازه مجاز اجرا (۹:۳۰ تا ۲۳:۳۰). اسکریپت متوقف شد.")
+    sys.exit()
+
 
 def get_driver():
     try:
