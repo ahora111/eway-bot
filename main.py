15fac5105758680cda9c67c534ba38fc88ffcea6
ahora111 <nimbuz72@gmail.com>
2025-07-18 12:57:26 +0330

    Update main.py

--- main.py
+++ main.py
@@ -38,10 +38,11 @@ if not (start_time <= current_time <= end_time):
 def get_driver():
     try:
         options = webdriver.ChromeOptions()
-        # options.add_argument("--headless")  # برای تست، این خط را کامنت کن
+        options.add_argument("--headless=new")  # یا فقط --headless
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
+        # هیچ user-data-dir اضافه نکن!
         service = Service()
         driver = webdriver.Chrome(service=service, options=options)
         return driver
