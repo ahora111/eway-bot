fe07d3cfeca38ece1f59dad9e488d6d49652c030
ahora111 <nimbuz72@gmail.com>
2025-08-09 17:25:00 +0330

    Update main.py

--- main.py
+++ main.py
@@ -801,7 +801,7 @@ def main():
         return
     init_category_index_global(all_cats)
 
-    SELECTED_IDS_STRING = os.environ.get("SELECTED_IDS_STRING") or "16777:all-allz"
+    SELECTED_IDS_STRING = os.environ.get("SELECTED_IDS_STRING") or "1582:14548-allz,1584-all-allz|16777:all-allz|4882:all-allz|16778:22570-all-allz"
     parsed_selection = parse_selected_ids_string(SELECTED_IDS_STRING)
 
     # انتخاب‌ها (بدون افزودن خودکار والد)
